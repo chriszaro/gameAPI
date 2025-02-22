bb53d68ab790faa1c8d75842979148558a282c21
Christodoulos Zarogiannis <chriszaro@gmail.com>
2025-02-22 15:26:47 +0200

    UPDATE request now returns updated record

--- app/Http/Controllers/Api/V1/GameController.php
+++ app/Http/Controllers/Api/V1/GameController.php
@@ -74,6 +74,7 @@ class GameController extends Controller
     public function update(UpdateGameRequest $request, Game $game)
     {
         $game->update($request->all());
+        return new GameResource($game);
     }
 
     /**
